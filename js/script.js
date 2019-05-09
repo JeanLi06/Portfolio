3db825c7e3f5eec25cfcd05d37f7ff2c6d2f18d1
Jean Liautaud <jliautaud@free.fr>
2019-05-09 14:20:01 +0200

    Add Gradients

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
-// Smooth scroll
-$(function () {
-    $('a[href*=#]:not([href=#])').click(function () {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({scrollTop: target.offset().top}, 1000);
-                return false;
-            }
-        }
-    });
-});
+
 
 // Pour chaque image dans main, on attache un attribut pour l'animation, si mobile
 if ($(window).width() < 767) {
     $('main section img:odd').attr('data-aos', 'fade-right');
     $('main section img:even').attr('data-aos', 'fade-left');
+    $('body').removeClass('container');
 }
+
 
 // Init de Animate On Scroll
 AOS.init();
+
+// Smooth scroll - Les liens du menu doivent avoir une classe "js-scrollTo"
+$(document).ready(function() {
+    $('.js-scrollTo').on('click', function() { // Au clic sur un élément
+        var page = $(this).attr('href'); // Page cible
+        var speed = 750; // Durée de l'animation (en ms)
+        $('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+        return false;
+    });
+});
